34ea8d2092a42779a429d790712c68153863f755
Sebastian <jsordonezs@unal.edu.co>
2021-02-24 16:43:09 -0500

    Added function for getting boxplots.

--- 07-Plant-Generated-Electricity/main.py
+++ 07-Plant-Generated-Electricity/main.py
@@ -1,12 +1,11 @@
 #Libraries
+import plotly
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 from os import listdir
 
-
-
-#Average voltage
+#Reading measurements->Average voltage
 def get_averages(folder:str)->list:
     """
     This function takes data, precisely voltage, from a given folder (named after
@@ -37,10 +36,22 @@ def get_averages(folder:str)->list:
     Voltages = Voltages.reshape(len(Voltages),int(len(Voltages[0])/5.), 5)
     #Taking averages for each set of measurements for each prototype
     averageProto = np.sum(Voltages, axis = 2)/5.
+    averageProto = np.transpose(np.array(averageProto))
     #Taking averages for the whole group
-    finalAverages = [round(jj,2) for jj in np.sum(averageProto, axis =0)/5.]
-    finalAverages = finalAverages[2:] #Delete first measurements, not reliable
 
+    #finalAverages = [round(jj,2) for jj in np.sum(averageProto, axis =1)/5.]
+    #finalAverages = finalAverages[2:] #Delete first measurements, not reliable
+
+    #return averageProto
+    return averageProto
+
+#print(get_averages("Living-Soil")) #testing
+
+
+#Statistics
+pd.options.plotting.backend = "plotly" #Necessary for boxplots (?)
+
+def get_boxPlot(avProto:np.array):
     """
     MISSING UNCERTAINTIES AND RELATED STATISTICS
         -> Show in our plots the whole ensemble statistics (Box plots + whyskers?)
@@ -49,9 +60,19 @@ def get_averages(folder:str)->list:
            comparing Plants_Light with Plants_No_Light.
         -> Quantize the effect of humidity for each experiment.
     """
-    return finalAverages
+    #avProto = np.array(avProto)
+    #df = pd.DataFrame(avProto)
+    boxes = plt.figure()
+    boxData = [x for x in avProto]
+    plt.boxplot(boxData)
+    plt.show()
+    #print(df)
+    return boxes
 
-#print(get_averages("Living-Soil")) #testing
+get_boxPlot(get_averages("Plants-Light"))
+#get_boxPlot(get_averages("Plants-No-Light"))
+
+
 
 #Plots
 def get_plots(**kwargs)->plt.figure:
@@ -76,7 +97,9 @@ def get_plots(**kwargs)->plt.figure:
 
     return final
 
+"""
 get_plots(Dead_Soil = get_averages("Dead-Soil"),
             Living_Soil = get_averages("Living-Soil"),
             Plants_Light = get_averages("Plants-Light"),
             Plants_No_Light = get_averages("Plants-No-Light"))
+"""
